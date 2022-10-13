f9a142cd9bd0cefa79b7ec3792dac3e85ffabb6d
Marko Varecha <varechamarko95@gmail.com>
2022-10-13 16:42:05 +0200

    added title

--- src/com/gfa/exam/Test.java
+++ src/com/gfa/exam/Test.java
@@ -4,6 +4,7 @@ public class Test {
    private int healthPoints ;
    private int attack;
    private boolean alive;
+   private String title ;
 
     public Test(int healthPoints, int attack, boolean alive) {
         this.healthPoints = healthPoints;
