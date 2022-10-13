ab5c8d510b90df99ff6fc96444f1159f9a7d3360
Marko Varecha <varechamarko95@gmail.com>
2022-10-13 16:34:44 +0200

    made some pseudo-class

---
+++ src/com/gfa/exam/Test.java
@@ -0,0 +1,14 @@
+package com.gfa.exam;
+
+public class Test {
+   private int healthPoints ;
+   private int attack;
+   private boolean alive;
+
+    public Test(int healthPoints, int attack, boolean alive) {
+        this.healthPoints = healthPoints;
+        this.attack = attack;
+        this.alive = alive;
+    }
+}
+
